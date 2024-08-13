a1674fc4b142f8b9c7fd78de648512d03b7e114d
abuobayda Shabat <abshabat@gmail.com>
2024-08-13 22:10:49 +0200

    Update views.py

--- api/cases/views.py
+++ api/cases/views.py
@@ -1280,7 +1280,11 @@ class CaseGetAllSortedResource(Resource):
                 if sort_field == 'serviceDate':
                     return datetime.strptime(case['beneficiaries'][0]['serviceDate'], "%d %b %Y") if case['beneficiaries'] and case['beneficiaries'][0].get('serviceDate') else datetime.min
                 elif sort_field == 'totalSupportCost':
-                    return float(case.get('beneficiaries', [{}])[0].get('totalSupportCost', 0))
+                    if case.get('beneficiaries'):
+                        # Use the first beneficiary's totalSupportCost if available
+                        return float(case['beneficiaries'][0].get('totalSupportCost', 0))
+                    else:
+                        return 0
                 elif sort_field == 'caseStatus':
                     return case.get('caseStatus', '')
                 else:
